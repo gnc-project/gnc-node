41751c27204317f3b5d7cd7a2ecdb69b13b3d6ea
cs2020 <linzhaojie@12.com>
2020-11-13 10:59:23 +0800

    best

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+mkdir ./data
+
 nohup ./gnc  --syncmode 'full' --cache 256 --datadir data --rpc  --rpcaddr 0.0.0.0 --rpcapi 'net,db,eth,web3,txpool' --rpcport 8545 --port 30303 --rpccorsdomain "*"  --rpcvhosts "*" --allow-insecure-unlock --networkid 37021 --ws --wsaddr 0.0.0.0 --wsport "8560" --wsapi "net,db,eth,web3,txpool" --wsorigins "*" >data/geth.log 2>&1 &
